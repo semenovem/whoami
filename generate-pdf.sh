63ff962efdf66cc614d77595f5f4298c2f37c7ac
Semenov Evgeny <semenovem@gmail.com>
2022-05-13 13:41:59 +0400

    upd last

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -2,8 +2,8 @@
 
 BIN=$(dirname "$([[ $0 == /* ]] && echo "$0" || echo "$PWD/${0#./}")")
 
-grep -v "скачать в PDF" "${BIN}/README.md" \
-  | node_modules/md-to-pdf/dist/cli.js \
-    --stylesheet	css/style.css \
+grep -v "скачать в PDF" "${BIN}/README.md" |
+  node_modules/md-to-pdf/dist/cli.js \
+    --stylesheet css/style.css \
     --pdf-options '{ "format": "Letter", "margin": "10mm", "printBackground": true }' \
-  > resume.pdf
+    >resume.pdf
